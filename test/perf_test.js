a156abbcd561892e9afa7e4ae9b6a0a340b8e631
Manuel Simoni <msimoni@gmail.com>
2012-10-24 14:36:27 +0200

    Update perf_test to use new API.

--- test/perf_test.js
+++ test/perf_test.js
@@ -1,4 +1,4 @@
-var avro = require("../build/Release/avro");
+var avro = require("../lib/index");
 var should = require("should");
 
 var visitorSchema =
@@ -21,7 +21,8 @@ var visitorSchema =
     ]
   };
 
-var schemaObj = avro.parseSchema(JSON.stringify(visitorSchema));
+var schemaObj = avro.prepareSchema(visitorSchema);
+
 var record = { "cookie_id": "1881c3fc349466de14f165e8604307e7",
                "segments": { "a": 122, "b": 244, "c": 366 } };
 
@@ -29,14 +30,14 @@ describe("Avro performance test", function() {
 
     it("test how many record it can encode per second", function() {
         for (var i = 0; i < 10000; i++) {
-            var buf = avro.jsonStringToAvroBuffer(schemaObj, JSON.stringify(record));
+            var buf = schemaObj.encode(record);
         }
     });
 
     it("test how many record it can decode per second", function() {
-        var buf = avro.jsonStringToAvroBuffer(schemaObj, JSON.stringify(record));
+        var buf = schemaObj.encode(record);
         for (var i = 0; i < 10000; i++) {
-            var decodedRecord = JSON.parse(avro.avroBufferToJsonString(schemaObj, buf));
+            var decodedRecord = schemaObj.decode(buf);
         }
     });
 
